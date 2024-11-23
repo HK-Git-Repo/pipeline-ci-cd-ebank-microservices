b1349f453d72eaf73a3228b85e22cec9b713d03a
HK-Git-Repo <hamza.elkaddari.pro@gmail.com>
2024-11-23 12:21:56 +0100

    add-customer-mapper

--- customer-service/src/main/java/kad/dev/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/kad/dev/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package kad.dev.customerservice;
 
+import kad.dev.customerservice.entities.Customer;
+import kad.dev.customerservice.mapper.CustomerMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,4 @@ public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
 }
